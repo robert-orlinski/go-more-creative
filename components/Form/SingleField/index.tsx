8c476fd4ae55c1810e5ef218dabdf49b1397c3b7
Robert Orliński <kontakt@robertorlinski.pl>
2021-05-21 17:26:57 +0200

    add aria-required and role="alert" tags to form fields

--- components/Form/SingleField/index.tsx
+++ components/Form/SingleField/index.tsx
@@ -10,8 +10,17 @@ export const SingleField: FC<FormFieldType> = ({ id, label, register, errors }) 
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <textarea {...register(name, { required: true })} className={styles.input} id={name} />
-      {errors[name] && <p className={styles.error}>you need to write down this idea 🚀</p>}
+      <textarea
+        aria-invalid={errors[name] ? 'true' : 'false'}
+        {...register(name, { required: true })}
+        className={styles.input}
+        id={name}
+      />
+      {errors[name] && (
+        <p role="alert" className={styles.error}>
+          you need to write down this idea 🚀
+        </p>
+      )}
     </>
   );
 };
