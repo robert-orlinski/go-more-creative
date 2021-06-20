ed7d4af40d36a780166e3d31e5f325d236e7afd4
Robert Orliński <kontakt@robertorlinski.pl>
2021-06-20 14:04:13 +0200

    remove strike placeholder

--- components/Nav/Points/index.tsx
+++ components/Nav/Points/index.tsx
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 import navStyles from '../Nav.module.scss';
 import styles from './Points.module.scss';
 
-import { FireIcon } from './Icons/FireIcon';
 import { StarIcon } from './Icons/StarIcon';
 
 import { SelectivelyVisibleElementType } from '../../../types/global';
@@ -32,10 +31,6 @@ export const Points: FC<SelectivelyVisibleElementType> = ({ visibleOnClassName }
 
   return (
     <ul className={classNames('flex unstyledList', visibleOnClassName)}>
-      <li className={classNames('flex alignCenter', navStyles.multipleGroup)}>
-        <FireIcon />
-        <span className={styles.number}>7</span>
-      </li>
       <li className={classNames('flex alignCenter', navStyles.multipleGroup)}>
         <StarIcon />
         <span className={styles.number}>{points}</span>
