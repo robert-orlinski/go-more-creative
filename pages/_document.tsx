1b74657aa0c0897aeec8ab843cbcb92d6663799d
Robert Orliński <kontakt@robertorlinski.pl>
2021-06-21 20:43:43 +0200

    add _document file to set <html> lang

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Main, Head, Html, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
