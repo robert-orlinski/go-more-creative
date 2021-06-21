074dd7de548192b1ceac01a3a8486deace79dfb4
Robert Orliński <kontakt@robertorlinski.pl>
2021-06-21 18:14:54 +0200

    add open graph to DefaultSeo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { Provider as StoreProvider } from 'react-redux';
 import { Provider as SessionProvider, getSession } from 'next-auth/client';
 import { DefaultSeo } from 'next-seo';
+import { useRouter } from 'next/router';
 
 import { meta } from '../helpers/metaData';
 
@@ -15,6 +16,8 @@ import { fetchEntries } from '../store/entriesSlice';
 import { fetchTopics } from '../store/topicsSlice';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { asPath } = useRouter();
+
   useEffect(() => {
     const setupStore = async () => {
       const session = await getSession();
@@ -31,7 +34,18 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <DefaultSeo title={meta.title} description={meta.description} />
+      <DefaultSeo
+        title={meta.title}
+        description={meta.description}
+        openGraph={{
+          type: 'website',
+          title: meta.title,
+          description: meta.description,
+          locale: 'pl_PL',
+          url: `https://${process.env.PUBLIC_URL!}${asPath}`,
+          site_name: meta.title,
+        }}
+      />
       <SessionProvider session={pageProps.session}>
         <StoreProvider store={store}>
           <Component {...pageProps} />
