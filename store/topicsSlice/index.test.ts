568177194f97f4a7807cbc64e5c58c7511bf67eb
Robert Orliński <kontakt@robertorlinski.pl>
2021-07-03 22:52:07 +0200

    minor fixes for topics slice test file

--- store/topicsSlice/index.test.ts
+++ store/topicsSlice/index.test.ts
@@ -19,21 +19,21 @@ beforeEach(() => {
 
 describe('initial state', () => {
   it('have initial state on first render', () => {
-    const entriesSliceData = testedStore.getState().topics;
+    const topicsSliceData = testedStore.getState().topics;
 
-    expect(entriesSliceData).toEqual(initialState);
+    expect(topicsSliceData).toEqual(initialState);
   });
 });
 
 describe('thunks', () => {
-  it('adds entries to the store on successful thunk resolve and displays proper message', async () => {
+  it('adds topics to the store on successful thunk resolve and displays proper message', async () => {
     fetch.mockResponse(JSON.stringify(mockedTopicsArray));
 
     await testedStore.dispatch(fetchTopics());
 
-    const entriesSliceData = testedStore.getState().topics;
+    const topicsSliceData = testedStore.getState().topics;
 
-    expect(entriesSliceData).toEqual({
+    expect(topicsSliceData).toEqual({
       list: mockedTopicsArray,
       currentTopic: mockedTopic,
       statusMessage: statusMessages.fulfilled,
@@ -45,17 +45,17 @@ describe('extra reducers', () => {
   it('returns initial state with pending message when pending extra reducer is at work', async () => {
     await testedStore.dispatch({ type: fetchTopics.pending.type });
 
-    const entriesSliceData = testedStore.getState().topics;
+    const topicsSliceData = testedStore.getState().topics;
 
-    expect(entriesSliceData).toEqual(initialState);
+    expect(topicsSliceData).toEqual(initialState);
   });
 
   it('returns initial state with fullfiled message when fullfiled extra reducer is at work', async () => {
     await testedStore.dispatch({ type: fetchTopics.fulfilled.type, payload: mockedTopicsArray });
 
-    const entriesSliceData = testedStore.getState().topics;
+    const topicsSliceData = testedStore.getState().topics;
 
-    expect(entriesSliceData).toEqual({
+    expect(topicsSliceData).toEqual({
       list: mockedTopicsArray,
       currentTopic: mockedTopic,
       statusMessage: statusMessages.fulfilled,
@@ -65,9 +65,9 @@ describe('extra reducers', () => {
   it('returns initial state with rejected message when rejected extra reducer is at work', async () => {
     await testedStore.dispatch({ type: fetchTopics.rejected.type });
 
-    const entriesSliceData = testedStore.getState().topics;
+    const topicsSliceData = testedStore.getState().topics;
 
-    expect(entriesSliceData).toEqual({
+    expect(topicsSliceData).toEqual({
       ...initialState,
       statusMessage: statusMessages.rejected,
     });
@@ -78,9 +78,9 @@ describe('actions', () => {
   it('selects a random topic as the current topic on "selectedRandomTopic" action', () => {
     testedStore.dispatch(selectedRandomTopic());
 
-    const entriesSliceData = testedStore.getState().topics;
-    expect(entriesSliceData).toEqual({
-      ...entriesSliceData,
+    const topicsSliceData = testedStore.getState().topics;
+    expect(topicsSliceData).toEqual({
+      ...topicsSliceData,
       currentTopic: mockedTopic,
     });
   });
