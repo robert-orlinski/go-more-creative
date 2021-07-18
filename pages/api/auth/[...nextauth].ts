d3a1c9614053195dafe182f62fee6e84d6a34e43
Robert Orliński <kontakt@robertorlinski.pl>
2021-07-18 16:32:26 +0200

    replace .env vars related to GH o-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.OAUTH_APP_ID,
+      clientSecret: process.env.OAUTH_APP_SECRET,
       scope: '',
     }),
   ],
